d56487fe7ab8b4e2496cfb591729eb52ea9b3b18
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-09-04 19:33:06 -0700

    added two other optimization approaches
    tof and horizontal error minimization

--- calculator.py
+++ calculator.py
@@ -20,7 +20,7 @@ launch_angle = 47.5
 max_launch_angle = 85.0
 
 # m 
-distance = 3
+distance = 2
 
 # m
 front_clearance = 0.2
@@ -34,6 +34,12 @@ top_clearance = 0.16
 # m (not sure what this is yet, have to change)
 shooter_height = 0.4
 
+# +/- x m/s
+velocity_uncertainty = 0.5
+
+# +/- x radians
+angle_uncertainty = 0.087
+
 plt.style.use('dark_background')
 
 def sec(theta):
@@ -46,7 +52,7 @@ def withinRange(v_0, theta, d, launch_height):
     discriminant = (v_0**2)*(np.sin(theta)**2)-4*(-0.5*g)*(launch_height-h)
     
     if discriminant < 0:
-        return False, None
+        return False, None, 0
     
     t_0 = (v_0*np.sin(theta)+np.sqrt(discriminant))/(g)
     t_1 = (distance)/(v_0*np.cos(theta))
@@ -55,7 +61,7 @@ def withinRange(v_0, theta, d, launch_height):
 
     x_position = x_pos(v_0, theta, t_0)
 
-    return (x_position >= (d+front_clearance) and x_position <= (d+w)-back_clearance and y_height >= (h+top_clearance)), t_0
+    return (x_position >= (d+front_clearance) and x_position <= (d+w)-back_clearance and y_height >= (h+top_clearance)), t_0, x_position
 
 # calculates the x-position using kinematics
 # v_0 is launch velocity, theta is launch angle in radians, t is time
@@ -79,6 +85,9 @@ def dxd0(x, v, theta):
 
     return numerator/denominator
 
+def dx(x, v, theta):
+    return (np.abs(dxdv(x, v, theta))*velocity_uncertainty)+(np.abs(dxd0(x, v, theta))*angle_uncertainty)
+
 fig, axes = plt.subplots(1, 2, figsize=(10, 6))
 
 axes[0].vlines(x=distance, ymin=0, ymax=h)
@@ -88,6 +97,11 @@ axes[0].hlines(y=1.7, xmin=distance, xmax=distance+w)
 angles = []
 min_speeds = []
 max_speeds = []
+
+opt_ang_tof = 0
+opt_speed_tof = 0
+
+min_tof = 10000000
 
 while launch_angle <= max_launch_angle:
 
@@ -104,6 +118,11 @@ while launch_angle <= max_launch_angle:
             t = np.linspace(0, inRange[1], 100)
 
             times.append(t)
+
+            if (inRange[1] < min_tof):
+                min_tof = inRange[1]
+                opt_ang_tof = launch_angle
+                opt_speed_tof = launch_speed
 
         launch_speed += 0.01
     
@@ -157,16 +176,47 @@ def difference_func(x):
     return difference[0]*(x**4)+difference[1]*(x**3)+difference[2]*(x**2)+difference[3]*(x)+difference[4]
 
 max = -10000
-optimal_angle = 47.5
+optimal_angle = min(angles)
 
-counter = 47.5
+counter = optimal_angle
+
+potential_angles = []
+potential_speeds = []
 
 while (counter <= 85):
+    potential_angles.append(counter)
+
+    curr = lower_bound(counter)
+    vertical_upper = upper_bound(counter)
+
+    potential_speeds.append(((vertical_upper-curr)/2)+curr)
+
     if (difference_func(counter) > max):
         max = difference_func(counter)
         optimal_angle = counter
 
     counter += 0.1
+
+max_case = -1000000000
+
+opt_ang_dx = 0
+opt_speed_dx = 0
+
+for i in range(len(potential_speeds)):
+    inRange = withinRange(potential_speeds[i], potential_angles[i], distance, shooter_height)
+
+    curr_dx = dx(inRange[2], potential_speeds[i], potential_angles[i])
+
+    m1 = inRange[2] - distance
+    m2 = (distance+w) - inRange[2]
+
+    worst_case = min(m1, m2) - curr_dx
+
+    if (worst_case > max_case):
+        max_case = worst_case
+        
+        opt_ang_dx = potential_angles[i]
+        opt_speed_dx = potential_speeds[i]
 
 curr = lower_bound(optimal_angle)
 vertical_upper = upper_bound(optimal_angle)
@@ -183,15 +233,30 @@ axes[1].plot(angles, max_speeds, 'o', linestyle="-", color='green')
 axes[1].plot(angles, min_speeds, 'o', linestyle="-", color='red')
 
 val = withinRange(optimal_speed, np.radians(optimal_angle), distance, shooter_height)[1]
+val2 = withinRange(opt_speed_dx, np.radians(opt_ang_dx), distance, shooter_height)[1]
+val3 = withinRange(opt_speed_tof, np.radians(opt_ang_tof), distance, shooter_height)[1]
 
 t = np.linspace(0, val, 100)
-
-print(val)
+t2 = np.linspace(0, val2, 100)
+t3 = np.linspace(0, val3, 100)
 
 x_optimal = optimal_speed*np.cos(np.radians(optimal_angle))*t
 y_optimal = shooter_height+(optimal_speed)*np.sin(np.radians(optimal_angle))*t+(-0.5*g)*(t**2)
 
+x_optimal2 = opt_speed_dx*np.cos(np.radians(opt_ang_dx))*t2
+y_optimal2 = shooter_height+(opt_speed_dx)*np.sin(np.radians(opt_ang_dx))*t2+(-0.5*g)*(t2**2)
+
+x_tof = opt_speed_tof*np.cos(np.radians(opt_ang_tof))*t3
+y_tof = shooter_height+(opt_speed_tof)*np.sin(np.radians(opt_ang_tof))*t3+(-0.5*g)*(t3**2)
+
 axes[0].plot(x_optimal, y_optimal, linewidth=3, color='blue')
+axes[0].plot(x_optimal2, y_optimal2, linewidth=3, color='orange')
+axes[0].plot(x_tof, y_tof, linewidth=3, color='yellow')
+
+axes[1].plot(opt_ang_dx, opt_speed_dx, color='orange', marker='o')
+
+print(opt_ang_dx, opt_speed_dx)
+print(min_tof)
 
 plt.fill_between(angles, min_speeds, max_speeds, color='purple', alpha=0.8)
 
