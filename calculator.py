7be40d3d3f370f60dd9e8e486d3f10357b884b92
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-09-05 13:57:17 -0700

    export data, data is very noisy and not consistent

--- calculator.py
+++ calculator.py
@@ -12,12 +12,12 @@ h = 1.8288
 g = 9.81
 
 # m/s
-launch_speed = 1.0
-max_launch_speed = 12
+# launch_speed = 1.0
+# max_launch_speed = 12
 
 # degrees
-launch_angle = 47.5
-max_launch_angle = 85.0
+# launch_angle = 47.5
+# max_launch_angle = 85.0
 
 # m 
 distance = 4
@@ -59,7 +59,7 @@ def withinRange(v_0, theta, d, launch_height):
         return False, None, 0
     
     t_0 = (v_0*np.sin(theta)+np.sqrt(discriminant))/(g)
-    t_1 = (distance)/(v_0*np.cos(theta))
+    t_1 = (d)/(v_0*np.cos(theta))
 
     y_height = y_pos(v_0, theta, launch_height, t_1)
 
@@ -94,169 +94,190 @@ def dx(x, v, theta):
 
 fig, axes = plt.subplots(1, 2, figsize=(10, 6))
 
-axes[0].vlines(x=distance, ymin=0, ymax=h)
-axes[0].vlines(x=distance+w, ymin=0, ymax=h)
-axes[0].hlines(y=1.7, xmin=distance, xmax=distance+w)
+def calculate_shot(current_distance):
+    launch_speed = 1.0
+    max_launch_speed = 12
 
-angles = []
-min_speeds = []
-max_speeds = []
+    launch_angle = 65
+    max_launch_angle = 75
 
-min_tof = 10000000
-max_tof = -1000000
+    axes[0].vlines(x=current_distance, ymin=0, ymax=h)
+    axes[0].vlines(x=current_distance+w, ymin=0, ymax=h)
+    axes[0].hlines(y=1.7, xmin=current_distance, xmax=current_distance+w)
 
-min_clearance = float('inf')
-max_clearance = float('-inf')
+    angles = []
+    min_speeds = []
+    max_speeds = []
 
-while launch_angle <= max_launch_angle:
+    min_tof = 10000000
+    max_tof = -1000000
 
-    speeds = []
-    times = []
+    min_clearance = float('inf')
+    max_clearance = float('-inf')
 
-    while launch_speed <= max_launch_speed:
+    while launch_angle <= max_launch_angle:
 
-        inRange = withinRange(launch_speed, np.radians(launch_angle), distance, shooter_height)
+        speeds = []
+        times = []
 
-        if inRange[0]:
-            speeds.append(launch_speed)
+        while launch_speed <= max_launch_speed:
 
-            t = np.linspace(0, inRange[1], 100)
+            inRange = withinRange(launch_speed, np.radians(launch_angle), current_distance, shooter_height)
 
-            times.append(t)
+            if inRange[0]:
+                speeds.append(launch_speed)
 
-            curr_dx = dx(inRange[2], launch_speed, np.radians(launch_angle))
+                t = np.linspace(0, inRange[1], 100)
+
+                times.append(t)
+
+                curr_dx = dx(inRange[2], launch_speed, np.radians(launch_angle))
             
-            m1 = inRange[2] - distance
-            m2 = (distance+w) - inRange[2]
+                m1 = inRange[2] - current_distance
+                m2 = (current_distance+w) - inRange[2]
             
-            clearance = min(m1, m2) - curr_dx
+                clearance = min(m1, m2) - curr_dx
 
-            # hub_center = distance + (w / 2.0)
-            # dist_from_center = np.abs(inRange[2] - hub_center)
+                # hub_center = distance + (w / 2.0)
+                # dist_from_center = np.abs(inRange[2] - hub_center)
 
-            # clearance = (w / 2.0) - dist_from_center - curr_dx
+                # clearance = (w / 2.0) - dist_from_center - curr_dx
 
-            if (inRange[1] < min_tof):
-                min_tof = inRange[1]
+                if (inRange[1] < min_tof):
+                    min_tof = inRange[1]
 
-            if (inRange[1] > max_tof):
-                max_tof = inRange[1]
+                if (inRange[1] > max_tof):
+                    max_tof = inRange[1]
 
-            if (clearance < min_clearance):
-                min_clearance = clearance
+                if (clearance < min_clearance):
+                    min_clearance = clearance
                 
-            if (clearance > max_clearance):
-                max_clearance = clearance
+                if (clearance > max_clearance):
+                    max_clearance = clearance
 
-        launch_speed += 0.01
+            launch_speed += 0.01
     
-    if len(speeds) != 0:     
-        min_velocity = min(speeds)
-        max_velocity = max(speeds)
+        if len(speeds) != 0:     
+            min_velocity = min(speeds)
+            max_velocity = max(speeds)
 
-        min_time = speeds.index(min_velocity)
-        max_time = speeds.index(max_velocity)
+            min_time = speeds.index(min_velocity)
+            max_time = speeds.index(max_velocity)
 
-        x1 = min_velocity*np.cos(np.radians(launch_angle))*times[min_time]
-        y1 = shooter_height+(min_velocity*np.sin(np.radians(launch_angle))*times[min_time])-(0.5*g)*(times[min_time]**2)
+            x1 = min_velocity*np.cos(np.radians(launch_angle))*times[min_time]
+            y1 = shooter_height+(min_velocity*np.sin(np.radians(launch_angle))*times[min_time])-(0.5*g)*(times[min_time]**2)
 
-        x2 = max_velocity*np.cos(np.radians(launch_angle))*times[max_time]
-        y2 = shooter_height+(max_velocity*np.sin(np.radians(launch_angle))*times[max_time])-(0.5*g)*(times[max_time]**2)
+            x2 = max_velocity*np.cos(np.radians(launch_angle))*times[max_time]
+            y2 = shooter_height+(max_velocity*np.sin(np.radians(launch_angle))*times[max_time])-(0.5*g)*(times[max_time]**2)
 
-        axes[0].plot(x1, y1, color='red', linewidth=0.3)
-        axes[0].plot(x2, y2, color='green', linewidth=0.3)
-
-        angles.append(launch_angle)
-        min_speeds.append(min_velocity)
-        max_speeds.append(max_velocity)
+            # axes[0].plot(x1, y1, color='red', linewidth=0.3)
+            # axes[0].plot(x2, y2, color='green', linewidth=0.3)
+# 
+            angles.append(launch_angle)
+            min_speeds.append(min_velocity)
+            max_speeds.append(max_velocity)
     
+        launch_speed = 1.0
+
+        launch_angle += 0.5
+
+    upper_coeff = np.polyfit(angles, max_speeds, 4)
+
+    lower_coeff = np.polyfit(angles, min_speeds, 4)
+
+    difference = upper_coeff-lower_coeff
+
+    # def upper_bound(x):
+    #     return upper_coeff[0]*(x**4)+upper_coeff[1]*(x**3)+upper_coeff[2]*(x**2)+upper_coeff[3]*(x)+upper_coeff[4]
+
+    # def lower_bound(x):
+    #     return lower_coeff[0]*(x**4)+lower_coeff[1]*(x**3)+lower_coeff[2]*(x**2)+lower_coeff[3]*(x)+lower_coeff[4]
+
+    def difference_func(x):
+        return difference[0]*(x**4)+difference[1]*(x**3)+difference[2]*(x**2)+difference[3]*(x)+difference[4]
+
+    max_margin = -10000
+    min_margin = 10000
+
     launch_speed = 1.0
+    launch_angle = min(angles)
 
-    launch_angle += 0.5
+    counter = min(angles)
 
-upper_coeff = np.polyfit(angles, max_speeds, 4)
+    while (counter <= 85):
 
-lower_coeff = np.polyfit(angles, min_speeds, 4)
+        if (difference_func(counter) > max_margin):
+            max_margin = difference_func(counter)
 
-difference = upper_coeff-lower_coeff
+        if (difference_func(counter) < min_margin):
+            min_margin = difference_func(counter)
 
-def upper_bound(x):
-    return upper_coeff[0]*(x**4)+upper_coeff[1]*(x**3)+upper_coeff[2]*(x**2)+upper_coeff[3]*(x)+upper_coeff[4]
+        counter += 0.1
 
-def lower_bound(x):
-    return lower_coeff[0]*(x**4)+lower_coeff[1]*(x**3)+lower_coeff[2]*(x**2)+lower_coeff[3]*(x)+lower_coeff[4]
+    max_score = -100000
+    score_speed = 0
+    score_angle = 0
 
-def difference_func(x):
-    return difference[0]*(x**4)+difference[1]*(x**3)+difference[2]*(x**2)+difference[3]*(x)+difference[4]
-
-max_margin = -10000
-min_margin = 10000
-
-launch_speed = 1.0
-launch_angle = min(angles)
-
-counter = min(angles)
-
-while (counter <= 85):
-
-    if (difference_func(counter) > max_margin):
-        max_margin = difference_func(counter)
-
-    if (difference_func(counter) < min_margin):
-        min_margin = difference_func(counter)
-
-    counter += 0.1
-
-max_score = -100000
-score_speed = 0
-score_angle = 0
-
-while launch_angle <= max_launch_angle:
-    while launch_speed <= max_launch_speed:
-        inRange = withinRange(launch_speed, np.radians(launch_angle), distance, shooter_height)
+    while launch_angle <= max_launch_angle:
+        while launch_speed <= max_launch_speed:
+            inRange = withinRange(launch_speed, np.radians(launch_angle), current_distance, shooter_height)
         
-        if inRange[0]:
-            curr_dx = dx(inRange[2], launch_speed, np.radians(launch_angle))
+            if inRange[0]:
+                curr_dx = dx(inRange[2], launch_speed, np.radians(launch_angle))
             
-            m1 = inRange[2] - distance
-            m2 = (distance+w) - inRange[2]
+                m1 = inRange[2] - current_distance
+                m2 = (current_distance+w) - inRange[2]
             
-            clearance = min(m1, m2) - curr_dx
+                clearance = min(m1, m2) - curr_dx
 
-            # hub_center = distance + (w / 2.0)
-            # dist_from_center = np.abs(inRange[2] - hub_center)
+                # hub_center = distance + (w / 2.0)
+                # dist_from_center = np.abs(inRange[2] - hub_center)
             
-            # clearance = (w / 2.0) - dist_from_center - curr_dx
+                # clearance = (w / 2.0) - dist_from_center - curr_dx
 
-            velocity_margin_score = (difference_func(launch_angle)-min_margin)/(max_margin-min_margin)
-            clearance_score = (clearance-min_clearance)/(max_clearance-min_clearance)
-            tof_score = (max_tof-inRange[1])/(max_tof-min_tof)
+                velocity_margin_score = (difference_func(launch_angle)-min_margin)/(max_margin-min_margin)
+                clearance_score = (clearance-min_clearance)/(max_clearance-min_clearance)
+                tof_score = (max_tof-inRange[1])/(max_tof-min_tof)
 
-            score = (vel_error_weight*velocity_margin_score)+(dx_weight*clearance_score)+(tof_weight*tof_score)
+                score = (vel_error_weight*velocity_margin_score)+(dx_weight*clearance_score)+(tof_weight*tof_score)
 
-            if score > max_score:
-                max_score = score
-                score_speed = launch_speed
-                score_angle = launch_angle
+                if score > max_score:
+                    max_score = score
+                    score_speed = launch_speed
+                    score_angle = launch_angle
 
-        launch_speed += 0.01
+            launch_speed += 0.01
 
-    launch_speed = 1.0
+        launch_speed = 1.0
 
-    launch_angle += 0.5
+        launch_angle += 0.5
 
-axes[1].plot(angles, max_speeds, 'o', linestyle="-", color='green')
-axes[1].plot(angles, min_speeds, 'o', linestyle="-", color='red')
+    # axes[1].plot(angles, max_speeds, 'o', linestyle="-", color='green')
+    # axes[1].plot(angles, min_speeds, 'o', linestyle="-", color='red')
 
-val = withinRange(score_speed, np.radians(score_angle), distance, shooter_height)[1]
+    val = withinRange(score_speed, np.radians(score_angle), current_distance, shooter_height)[1]
 
-t = np.linspace(0, val, 100)
+    t = np.linspace(0, val, 100)
 
-x_optimal3 = score_speed*np.cos(np.radians(score_angle))*t
-y_optimal3 = shooter_height+(score_speed)*np.sin(np.radians(score_angle))*t+(-0.5*g)*(t**2)
+    x_optimal3 = score_speed*np.cos(np.radians(score_angle))*t
+    y_optimal3 = shooter_height+(score_speed)*np.sin(np.radians(score_angle))*t+(-0.5*g)*(t**2)
 
-axes[0].plot(x_optimal3, y_optimal3, linewidth=3, color='yellow')
+    axes[0].plot(x_optimal3, y_optimal3, linewidth=3, color='yellow')
 
-plt.fill_between(angles, min_speeds, max_speeds, color='purple', alpha=0.8)
+    # axes[1].fill_between(angles, min_speeds, max_speeds, color='purple', alpha=0.8)
+
+    return f"distance: {current_distance} m \nangle: {score_angle} deg \nspeed: {round(score_speed, 3)} m/s\n"
+
+j = 1
+
+while (j <= 6):
+    if j == 1:
+        with open("shots.txt", "w", encoding="utf-8") as file:
+            file.write(calculate_shot(j))
+    else:
+        with open("shots.txt", "a", encoding="utf-8") as file:
+            file.write("\n" + calculate_shot(j))
+
+    j += 0.1
 
 plt.show()
